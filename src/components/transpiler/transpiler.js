6c620a9cfe8c246c898baa65d8e63256c4c75564
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2024-03-24 22:04:29 -0400

    chore: add source code

---
+++ src/components/transpiler/transpiler.js
@@ -0,0 +1,129 @@
+/* Helper Functions */
+
+// Count the total lines in the input code
+function getTotalLines(tokens) {
+  let lines = 1;
+  for (let i = 0; i < tokens.length; i++) {
+    if (tokens[i].value === "\n") lines++;
+  }
+  return lines;
+}
+
+// Returns the lines of code in a two-dimensional array
+function getLines(tokens, total_lines) {
+  let lines = [];
+  let list = [];
+  let index = 0;
+  for (let j = 0; j < total_lines; j++) {
+    for (index; index < tokens.length; index++) {
+      list.push(tokens[index].value);
+      if (tokens[index].value === "\n") {
+        index++;
+        break;
+      }
+    }
+    lines.push(list);
+    list = [];
+  }
+  return lines;
+}
+
+// Returns an array to measure the level of indentation of each line
+function getTabs(lines) {
+  let tabs = [];
+  let aux = 0;
+  for (let i = 0; i < lines.length; i++) {
+    aux = 0;
+    for (let j = 0; j < lines[i].length; j++) {
+      if (lines[i][j] === "  ") {
+        aux++;
+      }
+    }
+    tabs.push(aux);
+    // aux = 0
+  }
+  tabs.push(aux);
+  tabs.push(0);
+  return tabs;
+}
+
+// Insert new block closure lines
+function insertNewLines(lines, tabs) {
+  let close_block = [];
+  let lvl = 0;
+  for (let i = 0; i < tabs.length; i++) {
+    if (tabs[i] > tabs[i + 1]) {
+      lvl = Math.abs(tabs[i + 1] - tabs[i]);
+      for (let j = 0; j < lvl; j++) {
+        close_block.push("  ".repeat(j));
+        close_block.push("}");
+        close_block.push("\n");
+        lines.splice(i + 1, 0, close_block);
+        close_block = [];
+      }
+    }
+  }
+  return lines;
+}
+
+// Adapt the specified loops to the new syntax
+function updateLoopSyntax(lines) {
+  let loops = ["if", "elif", "while"];
+  for (let i = 0; i < lines.length; i++) {
+    for (let j = 0; j < lines[i].length; j++) {
+      for (let k = 0; k < loops.length; k++) {
+        if (lines[i][j] === loops[k]) {
+          lines[i].splice(j + 2, 0, "(");
+          lines[i].splice(lines[i].length - 2, 0, ")");
+        }
+      }
+    }
+  }
+  return lines;
+}
+
+// Makes literal substitutions in the incoming code
+function literalChanges(lines) {
+  for (let i = 0; i < lines.length; i++) {
+    for (let j = 0; j < lines[i].length; j++) {
+      // console.log(lines[i][j])
+      if (lines[i][j] === "def") lines[i][j] = "function";
+      if (lines[i][j] === "print") lines[i][j] = "console.log";
+      if (lines[i][j] === "elif") lines[i][j] = "else if";
+      if (lines[i][j] === ":") lines[i][j] = "{";
+      if (lines[i][j] === "True") lines[i][j] = "true";
+      if (lines[i][j] === "False") lines[i][j] = "false";
+      if (lines[i][j] === "except") lines[i][j] = "catch";
+      if (lines[i][j] === "==") lines[i][j] = "===";
+      if (lines[i][j] === "!=") lines[i][j] = "!==";
+      if (lines[i][j].startsWith("#")) {
+        let comment = lines[i][j].split("#");
+        lines[i][j] = "//" + comment[1];
+      }
+    }
+  }
+  return lines;
+}
+
+// Convert a two-dimensional array into an ordered string
+function arrToStr(lines) {
+  let out = "";
+  for (let i = 0; i < lines.length; i++) {
+    for (let j = 0; j < lines[i].length; j++) {
+      out += lines[i][j];
+    }
+  }
+  return out;
+}
+
+module.exports = function toJavascript(pyTokens) {
+  let total_lines = getTotalLines(pyTokens);
+  let lines = getLines(pyTokens, total_lines);
+  let tabs = getTabs(lines);
+  // Line analysis
+  lines = insertNewLines(lines, tabs);
+  lines = updateLoopSyntax(lines);
+  // Literal substitution
+  lines = literalChanges(lines);
+  return arrToStr(lines);
+};
---
+++ src/views/index.js
@@ -0,0 +1,122 @@
+const Swal = require("sweetalert2");
+const fs = require("fs");
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
+const pyScan = require("../components/scanner/python_analyzer");
+const pyParser = require("../components/parser/python_parser");
+const pythonShell = require("../components/shell/python");
+const transpile = require("../components/transpiler/transpiler");
+
+/* CodeMirror Window */
+// Python
+const myPythonCode = CodeMirror(document.getElementById("python-code"), {
+  mode: "python",
+  lineNumbers: true,
+  theme: "nord",
+});
+// Javascript
+const myJsCode = CodeMirror(document.getElementById("kotlin-code"), {
+  mode: "javascript",
+  lineNumbers: true,
+  theme: "nord",
+});
+
+/*   Lexical/Syntax Analysis   */
+async function callTranspiler() {
+  let py_code = myPythonCode.getValue();
+  // Lexical
+  let tokens = pyScan(py_code);
+  // console.log(tokens);
+  // Syntactic
+  let ast = await pyParser(tokens, false, false);
+  let syntax_errors = ast[0];
+  if (!syntax_errors) {
+    let outCode = transpile(tokens);
+    setJsValue(outCode);
+    // console.log(ast[1]);
+  } else {
+    Swal.fire({
+      type: "error",
+      title: "Oops...",
+      text: ast[1],
+    });
+  }
+}
+
+/* Shells execution */
+async function runPython() {
+  let code = myPythonCode.getValue();
+  pythonShell(code);
+}
+
+async function runJS() {
+  const code = await exec("node OutCode.js", { cwd: "examples" });
+  return { code };
+}
+
+/* Set editor text */
+function setJsValue(text) {
+  myJsCode.setValue(text);
+}
+
+function setpyvalue(text) {
+  myPythonCode.setValue(text);
+}
+
+/* Select Language Arrow */
+var band = 0;
+function arrow() {
+  if (band === 0) {
+    document.getElementById("icon").style.transform = "rotateZ(-180deg)";
+    band = 1;
+  } else {
+    document.getElementById("icon").style.transform = "rotateZ(0deg)";
+    band = 0;
+  }
+}
+
+function select_scanner() {
+  // Change 1 to 0 when everything is ready
+  if (band === 0) {
+    callTranspiler();
+  }
+}
+function runCode() {
+  if (band === 1) {
+    runPython();
+  } else {
+    runJS().then(function (e) {
+      document.getElementById("shell-area").value = e.code["stdout"];
+    });
+  }
+}
+
+/* Import py or kt files */
+function importpy() {
+  let file = document.getElementById("imp_py").files[0];
+  document.getElementById("imp_py").value = "";
+  let reader = new FileReader();
+  reader.readAsText(file, "UTF-8");
+  reader.onload = (readerEvent) => {
+    let content = readerEvent.target.result;
+    setpyvalue(content);
+  };
+}
+
+function exportJs() {
+  let js_code = myJsCode.getValue();
+  fs.writeFile("examples/OutCode.js", js_code, (err) => {
+    if (err) throw err;
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+
+    Toast.fire({
+      type: "success",
+      title: "File Exported!",
+    });
+  });
+}
